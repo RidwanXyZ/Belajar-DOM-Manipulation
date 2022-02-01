253e31b8e050b9575c7b9d93b6bd2a6e41b03cc9
OneTzy <54070834+RidwanXyZ@users.noreply.github.com>
2022-02-01 10:45:07 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+setInterval(jam(), 1000);
+function jam(){
+  w = new Date();
+  j = w.getHours();
+  m = w.getMinutes();
+  d = w.getSeconds();
+  
+  document.getElementById('clock').innerHTML = `<code>${j}:${m}:${d}</code>`
+}
